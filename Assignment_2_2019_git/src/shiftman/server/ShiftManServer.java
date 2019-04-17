7dabdf2f6b42acb5d1637734a5a2d3d0120e071d
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2019-04-17 14:46:29 +1200

    Implemented null return methods
    ShiftManServer implements ShiftMan, so I added null return methods for every inherited method for ShiftManServer.

--- Assignment_2_2019_git/src/shiftman/server/ShiftManServer.java
+++ Assignment_2_2019_git/src/shiftman/server/ShiftManServer.java
@@ -1,9 +1,57 @@
 package shiftman.server;
 
-public class ShiftManServer {
+import java.util.List;
+
+public class ShiftManServer implements ShiftMan {
 
 	public ShiftManServer() {
 		// TODO Auto-generated constructor stub
+		
 	}
-
+	
+	public String newRoster(String shopName) {
+		return null;
+	}
+	public String setWorkingHours(String dayOfWeek, String startTime, String endTime) {
+		return null;
+	}
+	public String addShift(String dayOfWeek, String startTime, String endTime, String minimumWorkers) {
+		return null;
+	}
+	public String registerStaff(String givenname, String familyName) {
+		return null;
+	}
+	public String assignStaff(String dayOfWeek, String startTime, String endTime, String givenName, String familyName, boolean isManager) {
+		return null;
+	}
+	public List<String> getRegisteredStaff(){
+		return null;
+	}
+	public List<String> getUnassignedStaff(){
+		return null;
+	}
+	public List<String> shiftsWithoutManagers(){
+		return null;
+	}
+	public List<String> understaffedShifts(){
+		return null;
+	}
+	public List<String> overstaffedShifts(){
+		return null;
+	}
+	public List<String> getRosterForDay(String dayOfWeek){
+		return null;
+	}
+	public List<String> getRosterForWorker(String workerName){
+		return null;
+	}
+	public List<String> getShiftsManagedBy(String managerName){
+		return null;
+	}
+	public String reportRosterIssues() {
+		return null;
+	}
+	public String displayRoster() {
+		return null;
+	}
 }
