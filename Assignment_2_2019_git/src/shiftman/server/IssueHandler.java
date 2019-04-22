785d396c043683409f229f67e54f75378e023fe9
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2019-04-23 00:59:37 +1200

    Renamed an exisiting interface to one more useful
    Refactoring - the sortable interface is a more relevant design than issue handler.

--- Assignment_2_2019_git/src/shiftman/server/IssueHandler.java
+++
@@ -1,5 +0,0 @@
-package shiftman.server;
-//May delete as the design has evolved past the point where this may be useful
-public interface IssueHandler {
-	
-}
---
+++ Assignment_2_2019_git/src/shiftman/server/Sortable.java
@@ -0,0 +1,5 @@
+package shiftman.server;
+//Renamed to another interface which will possibly be more useful
+public interface Sortable {
+	
+}
