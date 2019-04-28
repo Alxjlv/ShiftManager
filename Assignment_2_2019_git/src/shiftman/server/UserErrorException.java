2f4e0e007bc05cf77499bcdebfbf820b189a8e36
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2019-04-28 13:35:15 +1200

    Added default serial version to UserErrorException
    Previously had been suppressed, decided to add it in just in case not doing so may lead to errors.

--- Assignment_2_2019_git/src/shiftman/server/UserErrorException.java
+++ Assignment_2_2019_git/src/shiftman/server/UserErrorException.java
@@ -1,11 +1,10 @@
 package shiftman.server;
 
-@SuppressWarnings("serial")
 public class UserErrorException extends Exception {
 	/**
 	 * 
 	 */
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	//private String _errorMessage;
 	public UserErrorException(String message) {
 		super(message);
